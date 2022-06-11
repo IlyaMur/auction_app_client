ddc34169bf521229d363e4d07ed9fcb8fda57363
Ilya Mur <ilyamur@hotmail.com>
2022-06-11 12:32:17 +0300

    add tag list in design show

--- pages/designs/show.vue
+++ pages/designs/show.vue
@@ -84,9 +84,7 @@
                 <!-- End Designer info -->
                 <!-- Designer Design Info -->
                 <ul class="details-side-meta font-14 fw-400">
-                  <design-like
-                    :design="design"
-                  />
+                  <design-like :design="design" />
                   <li class="d-table w-100 author-previews-border mt-1">
                     <div class="stats-txt d-table-cell w-100">
                       Больше работ от {{ design.user.name }}:
@@ -108,16 +106,14 @@
                 <!-- Designs Tags -->
                 <div class="designs-tag-outer mt-3">
                   <h2 class="font-16 fw-500 mb-2">Tags</h2>
-                  <div class="designs-tag font-14 fw-300">
-                    <a href="#" title="3D">3D</a>
-                    <a href="#" title="among trees">among trees</a>
-                    <a href="#" title="birds">birds</a>
-                    <a href="#" title="environment">environment</a>
-                    <a href="#" title="forest">forest</a>
-                    <a href="#" title="night">night</a>
-                    <a href="#" title="stylized">stylized</a>
-                    <a href="#" title="sunset">sunset</a>
-                    <a href="#" title="survival">survival</a>
+                  <div class="designs-tag font-14 fw-300" v-if="design.tag_list">
+                    <a
+                      v-for="(tag, i) in design.tag_list.tags"
+                      :key="`tag-${i}`"
+                      :href="`/tags/${design.tag_list.normalized[i]}`"
+                    >
+                      {{tag}}</a
+                    >
                   </div>
                 </div>
                 <!-- End Designs Tags -->
